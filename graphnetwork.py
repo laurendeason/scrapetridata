c0c61720a96621b05cd85851bb8ccf05920db967
Lauren Deason <laurendeason@gmail.com>
2015-10-09 15:20:10 -0400

    Added graphnetwork python and ipython notebook files

--- graphnetwork.py
+++ graphnetwork.py
@@ -1,14 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Oct  7 13:01:49 2015
 
-@author: lauren
-"""
+# coding: utf-8
 
-#This function will take in a dataframe of triathlon results and generate a graph where nodes are individuals 
-#(as identified by Firstname, Lastname, Yearborn) and nodes are connected by weighted edges with weights 
-#given by the number of races in which individuals have both competed.
+# In[75]:
 
+from IPython import get_ipython  #This should allow exported .py file to run as python script
+get_ipython().magic(u'matplotlib inline')
 import networkx as nx
+import matplotlib.pyplot as plt
+import pandas as pd
 
-G = nx.Graph()
+
+# In[76]:
+
+allresults = pd.read_csv('./results/allresults.csv') 
+
+athletenodes = allresults[['firstname','lastname','yearborn','racecode','Year']] 
+racenodes = allresults[['firstname','lastname','yearborn','racecode','Year']] 
+#athletenodes.loc[:,'athlete'] = athletenodes['firstname']+athletenodes['lastname']+athletenodes['yearborn'].astype(str)
+#athletegraph_df = athletenodes.merge(athletenodes,how='inner',on=['racecode','Year']) 
+racenodes.loc[:,'race'] = racenodes['racecode']+racenodes['Year'].astype(str)
+racegraph_df = racenodes.merge(racenodes,how='inner',on=['firstname','lastname','yearborn']) 
+racegraph_df.head()
+
+
+# In[77]:
+
+racegraph_series_wt = racegraph_df.groupby(['race_x','race_y'])['firstname'].count()
+racegraph_df_wt = racegraph_series_wt.reset_index()
+racegraph_df_wt.columns = ['race_x','race_y','weight']
+racegraph_df_wt.head()
+
+
+# In[78]:
+
+nodecolordict = {'CH': 'green',
+              'DC': 'blue',
+              'NY': 'red'}
+
+racegraph = nx.from_pandas_dataframe(racegraph_df_wt, 'race_x', 'race_y', edge_attr=True)
+node_color = [nodecolordict[node[0:2]] for node in racegraph]
+node_size = [racegraph_df_wt.loc[(racegraph_df_wt['race_x']==node) & (racegraph_df_wt['race_y']==node)]['weight'].values[0] for node in racegraph]
+
+#pos = nx.spring_layout(racegraph)
+nx.draw(racegraph, node_color=node_color, node_size=node_size, with_labels=True)
+
+#nx.draw_networkx_labels(racegraph,pos)
+plt.show()
+
+
+# In[79]:
+
+nx.degree(racegraph)
+
