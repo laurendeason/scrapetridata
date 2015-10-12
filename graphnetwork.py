4d338f520d2b546111d5dccc408c85fe526a422d
Lauren Deason <laurendeason@gmail.com>
2015-10-12 17:38:14 -0400

    Updated graphnetwork based on new racecode format.

--- graphnetwork.py
+++ graphnetwork.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[75]:
+# In[2]:
 
 from IPython import get_ipython  #This should allow exported .py file to run as python script
 get_ipython().magic(u'matplotlib inline')
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-# In[76]:
+# In[4]:
 
 allresults = pd.read_csv('./results/allresults.csv') 
 
@@ -18,28 +18,32 @@ athletenodes = allresults[['firstname','lastname','yearborn','racecode','Year']]
 racenodes = allresults[['firstname','lastname','yearborn','racecode','Year']] 
 #athletenodes.loc[:,'athlete'] = athletenodes['firstname']+athletenodes['lastname']+athletenodes['yearborn'].astype(str)
 #athletegraph_df = athletenodes.merge(athletenodes,how='inner',on=['racecode','Year']) 
-racenodes.loc[:,'race'] = racenodes['racecode']+racenodes['Year'].astype(str)
 racegraph_df = racenodes.merge(racenodes,how='inner',on=['firstname','lastname','yearborn']) 
 racegraph_df.head()
 
 
-# In[77]:
+# In[19]:
 
-racegraph_series_wt = racegraph_df.groupby(['race_x','race_y'])['firstname'].count()
+racegraph_df.loc[(racegraph_df['racecode_x']=='CH_2004') & (racegraph_df['racecode_y']=='DC_2011')]
+
+
+# In[11]:
+
+racegraph_series_wt = racegraph_df.groupby(['racecode_x','racecode_y'])['firstname'].count()
 racegraph_df_wt = racegraph_series_wt.reset_index()
-racegraph_df_wt.columns = ['race_x','race_y','weight']
-racegraph_df_wt.head()
+racegraph_df_wt.columns = ['racecode_x','racecode_y','weight']
+racegraph_df_wt[:20]
 
 
-# In[78]:
+# In[22]:
 
 nodecolordict = {'CH': 'green',
               'DC': 'blue',
               'NY': 'red'}
 
-racegraph = nx.from_pandas_dataframe(racegraph_df_wt, 'race_x', 'race_y', edge_attr=True)
+racegraph = nx.from_pandas_dataframe(racegraph_df_wt, 'racecode_x', 'racecode_y', edge_attr=True)
 node_color = [nodecolordict[node[0:2]] for node in racegraph]
-node_size = [racegraph_df_wt.loc[(racegraph_df_wt['race_x']==node) & (racegraph_df_wt['race_y']==node)]['weight'].values[0] for node in racegraph]
+node_size = [racegraph_df_wt.loc[(racegraph_df_wt['racecode_x']==node) & (racegraph_df_wt['racecode_y']==node)]['weight'].values[0] for node in racegraph]
 
 #pos = nx.spring_layout(racegraph)
 nx.draw(racegraph, node_color=node_color, node_size=node_size, with_labels=True)
@@ -48,7 +52,17 @@ nx.draw(racegraph, node_color=node_color, node_size=node_size, with_labels=True)
 plt.show()
 
 
-# In[79]:
+# In[23]:
 
-nx.degree(racegraph)
+nx.edges(racegraph)
+
+
+# In[26]:
+
+nx.degree(racegraph, weight='weight')  #without including weight here, sees to count even missing edges
+
+
+# In[ ]:
+
+
 
