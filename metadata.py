628158ac1ad7936d82c24c4541da270d71a36543
Lauren Deason <laurendeason@gmail.com>
2015-10-12 17:38:47 -0400

    Updated to reflect new format of triclass Race

--- metadata.py
+++ metadata.py
@@ -10,112 +10,99 @@ import triclass as tri
 
 #future generalization - have dictionary defining unique code for each race, perhaps 2 digit alphanumeric, DC1, DC2, NY, CH, etc.  Eventually a df giving, for each race, set of divisions, race date, race lengths, etc.
 
-#create empty dictionaries of urls and column names
-#url = {}
-#columns = {}
-#
-#url['chicago'] =  { 2014: "http://results.active.com/events/transamerica-chicago-triathlon/international/expanded?",
-#                    2013: "http://results.active.com/events/life-time-chicago-triathlon-results--2/international/expanded?",
-#                    2012: "http://results.active.com/events/2012-chicago-triathlon-fleet-feet-super-sprint-and-chicago-kids-triathlon/international-individual/expanded?",
-#                    2011: "http://results.active.com/events/life-time-chicago-triathlon-results/international-individual/expanded?",
-#                    2010: "http://results.active.com/events/chicago-triathlon-results--2/international-individual/expanded?",
-#                    2009: "http://results.active.com/events/chicago-triathlon-results--3/international-individual/expanded?",
-#                    2008: "http://results.active.com/events/accenture-chicago-triathlon-mcdonald-s-kids-triathlon-and-fleet-feet-supersprint-triathlon/international-individual--3/expanded?",
-#                    2005: "http://results.active.com/events/chicago-triathlon-results/international-individual/expanded?",
-#                    2004: "http://results.active.com/events/chicago-triathlon-resuls/international-individual/expanded?"
-#                   }
-#               
-#columns['chicago'] =  { 2014 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'DIVISION', 'swim', 'bike', 'run', 'DIVplace', 'SEXplace', 'SWIMRANK', 'BIKERANK', 'MPH', 'RUNRANK', 'PENALTY', 'FinishTime', 'T1', 'T2'],
-#                        2013 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'DIVISION', 'swim', 'bike', 'run', 'DIVplace', 'SEXplace', 'SWIMRANK', 'BIKERANK', 'MPH', 'RUNRANK', 'FinishTime', 'T1', 'T2'],
-#                        2012 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-#                        2011 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-#                        2010 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-#                        2009 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-#                        2008 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-#                        2005 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-#                        2004 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],                   
-#                       }
-#
-#columns['dc'] =      {  2014 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
-#                        2013 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
-#                        2012 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],   
-#                      }
-#
-#url['dc']= {}
-#for year in columns['dc'].keys():                     
-#    url['dc'][year] = "http://nationstri.com/results/" + str(year) + "-results/"  
+CHyearlist = [2004,2005,2008,2009,2010,2011,2012,2013,2014]
+CHurldict =  { 2014: "http://results.active.com/events/transamerica-chicago-triathlon/international/expanded?",
+               2013: "http://results.active.com/events/life-time-chicago-triathlon-results--2/international/expanded?",
+               2012: "http://results.active.com/events/2012-chicago-triathlon-fleet-feet-super-sprint-and-chicago-kids-triathlon/international-individual/expanded?",
+               2011: "http://results.active.com/events/life-time-chicago-triathlon-results/international-individual/expanded?",
+               2010: "http://results.active.com/events/chicago-triathlon-results--2/international-individual/expanded?",
+               2009: "http://results.active.com/events/chicago-triathlon-results--3/international-individual/expanded?",
+               2008: "http://results.active.com/events/accenture-chicago-triathlon-mcdonald-s-kids-triathlon-and-fleet-feet-supersprint-triathlon/international-individual--3/expanded?",
+               2005: "http://results.active.com/events/chicago-triathlon-results/international-individual/expanded?",
+               2004: "http://results.active.com/events/chicago-triathlon-resuls/international-individual/expanded?"
+               }
+CHcolnamedict = { 2014 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'DIVISION', 'swim', 'bike', 'run', 'DIVplace', 'SEXplace', 'SWIMRANK', 'BIKERANK', 'MPH', 'RUNRANK', 'PENALTY', 'FinishTime', 'T1', 'T2'],
+                  2013 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'DIVISION', 'swim', 'bike', 'run', 'DIVplace', 'SEXplace', 'SWIMRANK', 'BIKERANK', 'MPH', 'RUNRANK', 'FinishTime', 'T1', 'T2'],
+                  2012 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
+                  2011 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
+                  2010 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
+                  2009 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
+                  2008 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
+                  2005 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
+                  2004 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],                   
+               }
 
-CH = tri.Triathlon('CH', 'Chicago', 'IL', "Transamerica Chicago Triathlon", 
-               yearlist = [2004,2005,2008,2009,2010,2011,2012,2013,2014],
-               urldict = { 2014: "http://results.active.com/events/transamerica-chicago-triathlon/international/expanded?",
-                           2013: "http://results.active.com/events/life-time-chicago-triathlon-results--2/international/expanded?",
-                           2012: "http://results.active.com/events/2012-chicago-triathlon-fleet-feet-super-sprint-and-chicago-kids-triathlon/international-individual/expanded?",
-                           2011: "http://results.active.com/events/life-time-chicago-triathlon-results/international-individual/expanded?",
-                           2010: "http://results.active.com/events/chicago-triathlon-results--2/international-individual/expanded?",
-                           2009: "http://results.active.com/events/chicago-triathlon-results--3/international-individual/expanded?",
-                           2008: "http://results.active.com/events/accenture-chicago-triathlon-mcdonald-s-kids-triathlon-and-fleet-feet-supersprint-triathlon/international-individual--3/expanded?",
-                           2005: "http://results.active.com/events/chicago-triathlon-results/international-individual/expanded?",
-                           2004: "http://results.active.com/events/chicago-triathlon-resuls/international-individual/expanded?"
-               },
-               colnamedict = { 2014 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'DIVISION', 'swim', 'bike', 'run', 'DIVplace', 'SEXplace', 'SWIMRANK', 'BIKERANK', 'MPH', 'RUNRANK', 'PENALTY', 'FinishTime', 'T1', 'T2'],
-                               2013 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'DIVISION', 'swim', 'bike', 'run', 'DIVplace', 'SEXplace', 'SWIMRANK', 'BIKERANK', 'MPH', 'RUNRANK', 'FinishTime', 'T1', 'T2'],
-                               2012 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-                               2011 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-                               2010 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-                               2009 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-                               2008 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-                               2005 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],
-                               2004 : ['Place', 'Bib', 'Name', 'City', 'State', 'Age', 'Gender', 'swim', 'bike', 'run', 'FinishTime'],                   
-               },
-               baseurl="http://www.chicagotriathlon.com/",
+#make CH now be a list tri races, one for each year
+CH = []
+for year in CHyearlist:
+    CH.append(tri.Race('CH_'+str(year), 'Chicago', 'IL', "Transamerica Chicago Triathlon", year, 
+               resulturl = CHurldict[year], 
+               columns = CHcolnamedict[year],  
                tableattributes = {'class':'participant-list'},
-               currpagecss = 'em.current' )
+               currpagecss = 'em.current',               
+               baseurl="http://www.chicagotriathlon.com/",
+               nameformat = 'Firstname Lastname'))
+for race in CH:
+    race.addnextpglink('css','a.next_page')  
+    race.addnextpglink('class','next_page')  
+    race.addnextpglink('text','Next →')
                
-CH.addnextpglink('css','a.next_page')  
-CH.addnextpglink('class','next_page')  
-CH.addnextpglink('text','Next →')          
+          
 
 #DC metadata
-DC = tri.Triathlon('DC', 'Washington', 'DC', "The Nation's Triathlon", 
-               yearlist = [2012,2013,2014,2015],
-               colnamedict = {  2015 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
-                                2014 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State',         'T1', 'bike', 'T2', 'run', 'FinishTime'],
-                                2013 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
-                                2012 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],   
-                      },
-               baseurl="http://nationstri.com/",
-               tableattributes = {'class':'data'},
-               currpagecss = 'p.rpp b',
-               nameformat = "Lastname, Firstname")
-
-for year in DC.yearlist:               
-    DC.addurl(year, "http://nationstri.com/results/" + str(year) + "-results/")
-
-DC.addnextpglink('text','>>')   
+DCyearlist = [2012,2013,2014,2015]
+DCcolnamedict = {  2015 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
+                   2014 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State',         'T1', 'bike', 'T2', 'run', 'FinishTime'],
+                   2013 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
+                   2012 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],   
+                }
+DC = []
+for year in DCyearlist:
+    DC.append(tri.Race('DC_'+str(year), 'Washington', 'DC', "The Nation's Triathlon", year, 
+         resulturl = "http://nationstri.com/results/" + str(year) + "-results/", 
+         columns = DCcolnamedict[year],
+         tableattributes = {'class':'data'},
+         currpagecss = 'p.rpp b',
+         nameformat = "Lastname, Firstname",
+         baseurl="http://nationstri.com/",
+         baseresulturl = "http://nationstri.com/results/"))
+for race in DC:
+  race.addnextpglink('text','>>')   
 
 #NY metadata
-NY = tri.Triathlon('NY', 'New York', 'NY', "New York City Triathlon", 
-               yearlist = [2012,2013,2014,2015],
-               colnamedict = {  2015 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
-                                2014 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
-                                2013 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
-                                2012 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],	
-                      },
-               baseurl="http://nyctri.com/",
-               resultsurl = 'http://www.nyctri.com/new-york/results/',
-               tableattributes = {'class':'data'},
-               currpagecss = 'p.rpp b',
-               nameformat = "Lastname, Firstname")
+NYyearlist = [2012,2013,2014,2015]
+NYcolnamedict = {  2015 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
+                   2014 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
+                   2013 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],
+                   2012 : ['Place', 'DIVISION', 'DIVplace', 'Name', 'Age', 'City', 'State', 'swim', 'T1', 'bike', 'T2', 'run', 'FinishTime'],  
+                }
+
+
+
+NY = []
+for year in NYyearlist:
+    NY.append(tri.Race('NY_'+str(year), 'New York', 'NY', "New York City Triathlon", year,
+         baseurl="http://nyctri.com/",
+         baseresulturl = 'http://www.nyctri.com/new-york/results/',
+         tableattributes = {'class':'data'},
+         currpagecss = 'p.rpp b',
+         nameformat = "Lastname, Firstname"))
+
 
 driver = webdriver.PhantomJS(port=65000)
 driver.set_window_size(1120, 550)
-for year in NY.yearlist: 
-    driver.get(NY.resultsurl)
-    url = driver.find_element_by_link_text(str(year)+' Results').get_attribute('href')            
-    NY.addurl(year, url)
+driver.get(NY[0].baseresulturl)  
+for race in NY:
+    race.addnextpglink('text','>>') 
+    race.resulturl = driver.find_element_by_link_text(str(race.year)+' Results').get_attribute('href')  
+
 driver.quit()
 
-NY.addnextpglink('text','>>')  
+trilistdict = {'CH' : CH,
+               'DC' : DC,
+               'NY' : NY } 
+
+
 
 
      
--- triclass.py
+++ triclass.py
@@ -4,44 +4,42 @@ Created on Tue Sep 22 16:41:23 2015
 
 @author: lauren
 """
+#not clear that making this a subclass is at all useful since i will be assigning code, etc for each event.  is there a way to declare the instance of the subclass taking on default
+#values from a particular instance of the class?  for now (10/12), getting rid of sublass idea; each race will be declared as an instance of the Race class, for now with racecode
+#indicating city and year, but to be amended in future to also indicate type of race (triathlon) and distance (i.e. CH_TRI_OLY_2008)
+
+#NEXT - decide about above, then continue editing below and update trisraper and metadata accordingly, then start getting older DC years
 
 #need to add in some attributes (possibly year specific) for the following:
-    #tableattributes -- table classname
     #format of times
-    #identifier for button to click through to next page
-    #currpagecss -- condition to check if page has updated correctly
-    #condition to indicate we are on last page of results for given year-race
 
-class Triathlon:
-    #Defines a racecode, city, state, and years in which it has triathlon results.  Optionally contains list of specific triathlons for that city and main url for that triathlon
-    #racecode is a unique identifier for the given race, usually a 2 letter code for the city in which it takes place
-    #baseurl is the url for the race overall (not the specific results pages)
-    #resultsurl is the url of a main results page which links to different years
-    #urldict is a dictionary with years as keys and urls for given results
-    #colnamedict is dictionary with years as keys and list of column names for results in table of given year
-    #tableattributes is a dictionary (for now assumed constant across all years) giving the value of given table attributes for the table of interest -- ultimately this should be optonal where code finds first table avail with certain characteristics(?)
+class Race():  
+    #racecode indicates city and year (i.e. CH_2008), but to be amended in future to also indicate type of race (triathlon) and distance (i.e. CH_TRI_OLYIND_2008)
+    #city is the full city name where the race takes place
+    #state is the full name of the state in which the race takes place (for US races, otherwise this gives country)
+    #fullname gives the full official name of the race
+    #year is the year in which the race takes place (may replace this with date in future, can get year from this)
+    #resultsurl is the year-race specific url where results for the given race are located
+    #columns is list of column names for results in table of given year
+    #tableattributes is a dictionary giving the value of given table attributes for the table of interest -- ultimately this should be optonal where code finds first table avail with certain characteristics(?)
     #currpagecss is a string for the css selector of the object that will contain in its text the unicode u`pagenum', this will be searched for and used to check that the page has updated before scraping    
     #nextpglink is a dictionary mapping keys text, css, class to the value of at least one of these.  Each will default to "missing"    
-    #nameformat is a string showing how the name field is formatted.  defaults to Firstname Lastname
-    def __init__(self, racecode, city, state, fullname, yearlist=None, urldict=None, colnamedict=None, baseurl = None, resultsurl=None, tableattributes=None, currpagecss=None, 
-                 nextpglink=None, nameformat=None):
-        #is it better to define url and colnames lists as dictionaries, or just lists that line up based on year? or maybe a full on dataframe?
+    #nameformat is a string showing how the name field is formatted.  defaults to separarated, meaning that Firstname and Lastname are already included as two separate columns
+    #baseurl (optional) gives the main website of the race (independent of year)
+    #baseresulturl (optional) gives the main results website for the race (independent of year)
+    def __init__(self, racecode, city, state, fullname, year, resulturl=None, columns=None, tableattributes=None, currpagecss=None, nextpglink=None, nameformat=None,
+        baseurl=None, baseresulturl=None ):
+        #is it better to define url and colnames lists as dictionaries, or just lists that line up based on year? or maybe a full on dataframe? as of
+        #10/9, changing format so that each instance is one city-race rather than the full set of races, so any lists or dictionaries will be defined at higher level
         # to have default values that are mutable objects, must be done as below.  might want to leave some defaults as None rather than putting in these misisng default values though
+        
         self.racecode = racecode
         self.city = city
         self.state = state
         self.fullname = fullname
-        if yearlist is None:
-            yearlist=[]
-        self.yearlist = yearlist
-        if urldict is None:
-            urldict={}
-        self.urldict = urldict
-        if colnamedict is None:
-            colnamedict={}
-        self.colnamedict = colnamedict
-        self.baseurl = baseurl
-        self.resultsurl = resultsurl
+        self.year = year
+        self.resulturl = resulturl
+        self.columns = columns
         if tableattributes is None:
             tableattributes={}
         self.tableattributes = tableattributes
@@ -50,30 +48,13 @@ class Triathlon:
             nextpglink = {"text": "missing", "css": "missing", "class": "missing"}
         self.nextpglink = nextpglink
         if nameformat is None:
-            nameformat = "Firstname Lastname"
+            nameformat = "separated"
         self.nameformat = nameformat
-        
-    def addurl(self,year,url):
-        self.urldict[year] = url
-        
-    def addcolnamelist(self,year,colnamelist):
-        self.colnamedict[year] = colnamelist
+        self.baseurl = baseurl
+        self.baseresulturl = baseresulturl
         
     def addnextpglink(self,key,val):
         self.nextpglink[key]=val
 
-##want to create a derived class for race that is uniquely identified by its 2 digit code, and the year - eventually code can be changed to include sprint vs. olympic etc
-#class TriRace(Triathlon):
-#    def __init__(self, raceid, city, year, url = 'nourl', columns = []):
-#        self.raceid = raceid
-#        self.city = city
-#        self.year = year
-#        self.url = url
-#        self.columns = columns
-        
-        
-#is it better to have class and subclass, or just the one class with attributes that are dictionaries?  for now, just pick one, move on
-        #with subclasses,first def would be  def __init__(self, racecode, city, fullname, yearlist=[], triraces = [], baseurl = 'nourl'): where
-        #triraces is a list of TriRace objects
 
         
